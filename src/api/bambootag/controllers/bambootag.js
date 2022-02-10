4a290a7170cb30e8ffc2fc2c61374a8972fdc101
DigitalTrans <digitaltrans@protonmail.com>
2022-02-10 16:36:27 +0530

    all tables

---
+++ src/api/bambootag/controllers/bambootag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  bambootag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bambootag.bambootag');
---
+++ src/api/bambootag/routes/bambootag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bambootag router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bambootag.bambootag');
